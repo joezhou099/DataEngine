f4cda3f6efbae1b40c808d6b91855a1876ec4bdd
joe <joe@YIs-MacBook-Air.local>
2021-02-12 16:17:18 +0800

    finished L2 action 1

--- L2_cousework/L2_cousework.py
+++ L2_cousework/L2_cousework.py
@@ -1,7 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
+# action 1
+# 加载
+train_data = pd.read_csv('./Titanic_Data/train.csv')
+test_data = pd.read_csv('./Titanic_Data/test.csv')
+
+# # 数据探索
+# print(train_data.info())
+# print('-'*30)
+# print(train_data.describe())
+# print('-'*30)
+# print(train_data.describe(include=['O']))
+# print('-'*30)
+
+# 预处理
+train_data['Age'].fillna(train_data['Age'].mean(), inplace=True)
+test_data['Age'].fillna(test_data['Age'].mean(),inplace=True)
+
+train_data['Fare'].fillna(train_data['Fare'].mean(), inplace=True)
+test_data['Fare'].fillna(test_data['Fare'].mean(),inplace=True)
+
+train_data['Embarked'].fillna('S', inplace=True)
+test_data['Embarked'].fillna('S',inplace=True)
+
+# 特征选择
+features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+train_features = train_data[features]
+train_labels = train_data['Survived']
+test_features = test_data[features]
+
+# 把字典中一些类别数据 Pclass Sex Embarked，分别进行转化成特征
+dvec=DictVectorizer(sparse=False)
+train_features=dvec.fit_transform(train_features.to_dict(orient='record'))
+test_features = dvec.fit_transform(test_features.to_dict(orient='record'))
+print(dvec.feature_names_)
+
+# 方法一 LOGISTIC REGRESSION
+clf1 = LogisticRegression(max_iter=100, verbose=True, random_state=33, tol=1e-4)
+clf1.fit(train_features, train_labels)
+acc1 = round(clf1.score(train_features, train_labels), 6)
+print(u'LOGISTIC REGRESSION score 准确率为 %.4lf' % acc1)
+pred1 = clf1.predict(test_features)
+
+# 方法二 SVM
+clf2 = SVC(C=10.0, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    tol=1e-4, verbose=False)
+clf2.fit(train_features, train_labels)
+acc2 = round(clf2.score(train_features, train_labels), 6)
+print(u'SVM score 准确率为 %.4lf' % acc2)
+pred2 = clf2.predict(test_features)
+
+# 方法三 决策树
+clf3 = DecisionTreeClassifier(criterion='entropy')
+clf3.fit(train_features, train_labels)
+acc3 = round(clf3.score(train_features, train_labels), 6)
+print(u'决策树 score 准确率为 %.4lf' % acc3)
+pred3 = clf3.predict(test_features)
+
+# 方法四 随机森林
+clf4 = RandomForestClassifier(n_estimators=200, max_depth=5)
+clf4.fit(train_features, train_labels)
+acc4 = round(clf4.score(train_features, train_labels), 6)
+print(u'随机森林score 准确率为 %.4lf' % acc4)
+pred4 = clf4.predict(test_features)
+
+
+
+# action 2
 def get_page_content(request_url):
     headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
     html=requests.get(request_url,headers=headers,timeout=10)
